86cb202a3637fe38becad173b156e3f1138b1205
eve1moms <ivvonekemunto4@gmail.com>
2023-04-20 10:45:58 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- *main-this is the main function
- *return-should always return 0
+ *main - this is the main function
+ *return - should always return 0
  */
 int main(void)
 {
