67f43490d8e9df7024694ac448bb7e40243a3200
eve1moms <ivvonekemunto4@gmail.com>
2023-04-20 10:40:12 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main-this is the main function 
- * return-should always return 0
+ *main-this is the main function
+ *return-should always return 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
