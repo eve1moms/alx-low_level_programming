1c33f9f819a8dd9ae759a5c57f503bee19af36ab
eve1moms <ivvonekemunto4@gmail.com>
2023-04-24 07:08:11 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and less than 6 and is not 0\n", n, nl);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nl);
 	}
 	return (0);
 }
