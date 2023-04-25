ebf2b39e52dd7d3c799a9e1979f9c6d3330b3f3e
eve1moms <ivvonekemunto4@gmail.com>
2023-04-25 16:33:09 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,10 @@ void print_alphabet_x10(void)
 	int n;
 	int m;
 
-	for (n = 1; n <= 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		for (m = 97; m <= 122; m++)
-		{
-			_putchar('m');
-		}
-		-putchar('\n');
+			_putchar(m);
+		_putchar('\n');
 	}
 }
