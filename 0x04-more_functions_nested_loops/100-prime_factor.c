03574bf3d9d28b52549037060cc245c1e8f3f635
eve1moms <ivvonekemunto4@gmail.com>
2023-04-27 11:55:44 -0700

    task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - prints large prime factor
+* Return: 0
+*/
+
+int main(void)
+{
+	long i, max;
+	long num = 612852475143;
+	double square = sqrt(num);
+
+	for (i = 1; i <= square; i++)
+	{
+		if (num % i == 0)
+		{
+			max = num / i;
+		}
+	}
+	printf("%ld\n", max);
+	return (0);
+}
