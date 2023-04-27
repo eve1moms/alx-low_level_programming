932ca17afb50f3fbd8a22331de2518e64fa5ab5b
eve1moms <ivvonekemunto4@gmail.com>
2023-04-27 06:20:20 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - prints numbers
+* @n: numbers
+*/
+
+void print_numbers(void)
+{
+	for (n >= '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
