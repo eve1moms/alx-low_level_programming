50784b7156914913f756a013ed8d9554b8797836
eve1moms <ivvonekemunto4@gmail.com>
2023-05-03 14:51:17 -0700

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,3 +7,34 @@
 
 int _atoi(char *s)
 {
+	int m, i, n, len, j, digit;
+
+	m = 0;
+	i = 0;
+	j = 0;
+	len = 0;
+	n = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[m] == '-')
+			i++;
+		if (s[m] >= '0' && s[m] <= '9')
+		{
+			digit = s[m] - '0';
+			if (i % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			j = 1;
+			if (s[m + 1] < '0' || s[m + 1] > '9')
+				break;
+			j = 0;
+		}
+		m++;
+	}
+	if (j == 0)
+		return (0);
+
+	return (n);
+}
