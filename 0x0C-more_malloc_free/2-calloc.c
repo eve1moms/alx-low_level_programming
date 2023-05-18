d70aba676bb9af3d26833ca15647b4c9cd22ff52
eve1moms <ivvonekemunto4@gmail.com>
2023-05-18 10:48:06 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,15 +7,15 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	char *a;
 	unsigned int n;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	a = malloc(nmemb * size);
+	if (a == NULL)
 		return (NULL);
 	for (n = 0; n < (nmemb = size); n++)
-		ptr[n] = 0;
-	return (ptr);
+		a[n] = 0;
+	return (a);
 }
