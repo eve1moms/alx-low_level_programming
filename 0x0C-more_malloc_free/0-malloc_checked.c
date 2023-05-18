deb3f20dc388178b83863782dac0a9a49ca9dcb2
eve1moms <ivvonekemunto4@gmail.com>
2023-05-18 17:18:57 +0300

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - afunction that allocates memory using malloc
+ * if the function fails it should cause normal process
+ * termination with a status value of 98
+ * @b: the allocated memory
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *n;
+
+	n = malloc(b);
+	if (n == NULL)
+		exit(98);
+	return(n);
+}
