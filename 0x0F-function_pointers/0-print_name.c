ed0553dee99cccd8f61f22bd6ce9ebab5b97e81b
eve1moms <ivvonekemunto4@gmail.com>
2023-05-24 07:12:52 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+* print_name - function that prints a name
+* @name: the name
+* @f: the function of name
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
