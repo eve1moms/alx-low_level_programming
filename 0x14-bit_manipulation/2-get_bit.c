fd62a5f89ccd224c337ec0aacee99ff86e2a1529
eve1moms <ivvonekemunto4@gmail.com>
2023-08-04 15:12:49 -0700

    task4;

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,15 +3,15 @@
  * get_bit - returns the value of a bit at an index in a decimal number
  * @n: number to search
  * @index: index of the bit
- * Return: value of the bit
+ * Return: value of the bit or -1 if fail
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_value;
+	int bitvalue;
 
 	if (index > 63)
 		return (-1);
 
-	bit_value = (n >> index) & 1;
-	return (bit_value);
+	bitvalue = (n >> index) & 1;
+	return (bitvalue);
 }
